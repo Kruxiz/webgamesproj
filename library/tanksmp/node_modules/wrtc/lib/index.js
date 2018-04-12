5e2e39de80bdff5c7c124a6af2735c5823e26353
Kruxiz <clarke1212@Hotmail.co.uk>
2018-04-12 18:22:39 +0100

    Huge Overhaul to include Tanks Games
    Loads of files changed and updated because of folder swapping issue with XAMPP hope to resolve soon.

---
+++ library/tanksmp/node_modules/wrtc/lib/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+exports.RTCDataChannel = require('./datachannel');
+exports.RTCDataChannelEvent = require('./datachannelevent');
+exports.RTCIceCandidate = require('./icecandidate');
+exports.RTCPeerConnection = require('./peerconnection');
+exports.RTCPeerConnectionIceEvent = require('./rtcpeerconnectioniceevent');
+exports.RTCSessionDescription = require('./sessiondescription');
