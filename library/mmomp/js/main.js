6e4a296f08f6d216ff76bb66ed90c3334e58a344
Kruxiz <clarke1212@Hotmail.co.uk>
2018-04-12 20:57:40 +0100

    Nav Bar and Sections Update
    Added new cleaner nav bar and drop down animated sections for the GDD.

--- library/mmomp/js/main.js
+++
@@ -1,3 +0,0 @@
-var game = new Phaser.Game(64*32, 30*32, Phaser.AUTO, "gameDiv");
-game.state.add('game',Game);
-game.state.start('game');
---
+++ library/tanksdatabase/js/mainMenu.js
@@ -0,0 +1,20 @@
+var main = function(){
+};
+	main.prototype = {
+		preload: function(){
+			game.load.image('background', 'library/tanksdatabase/assets/tanks/background.jpg');
+		},
+		create: function(){
+			game.add.sprite(0,0,'background');
+			var mainText = game.add.text(250,100,'Welcome to tanks!', { font: "20px Arial", fill: "#ffffff", align: "left" });
+			var mainText2 = game.add.text(250,200,'Click here to start!', { font: "20px Arial", fill: "#ffffff", align: "left" });
+			
+			mainText2.inputEnabled = true;
+			
+			mainText2.events.onInputUp.add(startGame);
+			
+			function startGame(){
+				game.state.start('gameFile');
+			}
+		}
+	};
